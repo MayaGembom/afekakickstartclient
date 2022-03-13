dcb7a3c56edfb9476b64e361ce910cc699db6c3d
MayaGembom <mayagembom3007@gmail.com>
2022-03-13 23:47:13 +0200

    fix css style

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { Grid, CircularProgress } from '@material-ui/core';
+import { Grid, CircularProgress, Divider } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 
 import Post from './Post/Post';
@@ -9,19 +9,50 @@ import useStyles from './styles';
 
 const Posts = ({ setCurrentId }) => {
 
-    const posts = useSelector((state) => state.posts);
-    const classes = useStyles();
-    
-    return (
-        !posts.length ? <CircularProgress /> : (
-          <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-            {posts.map((post) => (
-              <Grid key={post._id}  item xs={12} sm={10} md={4} lg={3}>
+  const posts = useSelector((state) => state.posts);
+  const classes = useStyles();
+  const currentDate = new Date();
+
+  return (
+
+    !posts.length ? <CircularProgress /> : (
+      <>
+        <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+          {posts.map((post) => (
+            (post?.pledgeCount < post?.goal) && (new Date(post?.deadLine) > currentDate) && (
+              <Grid key={post._id} item xs={12} sm={10} md={4} lg={3}>
                 <Post post={post} setCurrentId={setCurrentId} />
               </Grid>
-            ))}
-          </Grid>
-        )
-      );
-    };
+            )
+          ))}
+        </Grid>
+        <Divider style={{ margin: '20px 0' }} />
+
+        <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+          {posts.map((post) => (
+            (post?.pledgeCount < post?.goal) && (new Date(post?.deadLine) < currentDate) && (
+              <Grid key={post._id} item xs={12} sm={10} md={4} lg={3}>
+                <Post post={post} setCurrentId={setCurrentId} />
+              </Grid>
+            )
+          ))}
+        </Grid>
+
+        <Divider style={{ margin: '20px 0' }} />
+
+        <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+          {posts.map((post) => (
+            (post?.pledgeCount >= post?.goal) && (
+              <Grid key={post._id} item xs={12} sm={10} md={4} lg={3}>
+                <Post post={post} setCurrentId={setCurrentId} />
+              </Grid>
+            )
+          ))}
+        </Grid>
+      </>
+    )
+
+
+  );
+};
 export default Posts;
